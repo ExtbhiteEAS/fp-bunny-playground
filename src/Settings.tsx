eec68356e85690ecce18610ce66785507fb81838
Extbhite <extbhite@ya.ru>
2024-06-10 13:39:19 +0300

    IMPORTANT

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -12,10 +12,16 @@ const user = UserStore.getCurrentUser()
 
 export default () => (<ScrollView>
     <FormSection>
-        <FormText>This plugin is not finished result of fakeProfile. This is playground plugin special for fakeProfile development for mobile.</FormText>
+        <FormText>This plugin is not finished result of fakeProfile, it'll coming soon in new repository, finished and full than this. This is just playground plugin special for fakeProfile development for mobile.</FormText>
         <FormText></FormText>
         <FormText>ID: {user.id}</FormText>
         <FormText>Username: {user.username}</FormText>
+        <FormRow
+            label = "GitHub"
+            leading = {<FormRow.Icon source={getAssetIDByName("Discord")} />}
+            trailing = {FormRow.Arrow}
+            onPress = {() => url.openDeeplink("https://github.com/sampathgujarathi/fakeProfile")}
+        />
         <FormRow
             label = "Discord Server"
             leading = {<FormRow.Icon source={getAssetIDByName("Discord")} />}
